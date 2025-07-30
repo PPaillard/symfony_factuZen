31e3bc92179d7d561eb645fccf6acb874539863f
PierreP <paillard.pierre@gmail.com>
2025-07-30 16:59:16 +0200

    Doctrine 2B Exo 1 & 2

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Repository\ClientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ClientController extends AbstractController
+{
+    #[Route('/clients', name: 'client_index')]
+    public function index(ClientRepository $clientRepository): Response
+    {
+        $clients = $clientRepository->findAll();
+        return $this->render('client/index.html.twig', [
+            "clients" => $clients
+        ]);
+    }
+
+    #[Route('/clients/{id}', name: "client_show")]
+    public function show(Client $client): Response
+    {
+        return $this->render('client/show.html.twig', [
+            "client" => $client
+        ]);
+    }
+}
